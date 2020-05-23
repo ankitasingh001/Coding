7cc36c8f660230d8cad40f74f6780ef2f9c3af0d
ankitasingh001 <ankitasinghchikki@gmail.com>
2020-05-23 22:44:39 +0530

    Full code with 60/100

--- Heaps (Priority Queue)/prob1.py
+++ Heaps (Priority Queue)/prob1.py
@@ -1,6 +1,9 @@
 ''' https://www.codechef.com/JULY17/problems/IPCTRAIN 
 
     Author @ankitasingh001 '''
+# Using dictionary for priority data
+from collections import defaultdict
+import sys
 
 ## UTILITY FUNCTIONS FOR MAIN CODE ##
 
@@ -54,19 +57,97 @@ class add_lec:
             else:
                 self.list.append([start,start+lectures-1])
         else:
-           len_list = len(self.list)
-           print(len_list)
-           index = find_index(self.list,len_list,start)
-           a,b = self.list[index-1][0],self.list[index-1][1]
-           c,d = start,start+lectures-1
-           if(not range_intersection(a,b,c,d)):
-               self.list.insert(index-1,[c,d])
-           while (range_intersection(a,b,c,d)):
-               r1,r2 = merge_ranges(a,b,c,d)
+            len_list = len(self.list)
+            index = find_index(self.list,len_list,start)
+            index_one = index
+            a,b = self.list[index_one-1][0],self.list[index_one-1][1]
+            c,d = start,start+lectures-1
+            added =False
+                
+
+            while (range_intersection(a,b,c,d)):
+                added = True
+                r1,r2 = merge_ranges(a,b,c,d)
+                del self.list[index_one-1] 
+                if(index_one+1>len(self.list) or index_one<=0):
+                    break
+                a,b = self.list[index_one-1][0],self.list[index_one-1][1]
+                c,d = r1,r2
             
+            if(added is True):
+                if r2>self.max_Range :
+                    insert_index = find_index(self.list,len(self.list),r1)
+                    self.total_sad += (r2-self.max_Range )*sadness
+                    self.list.insert(insert_index,[r1,self.max_Range])
+                else:
+                    insert_index = find_index(self.list,len(self.list),r1)
+                    self.list.insert(insert_index,[r1,r2])
 
-l = [[1,3],[5,6],[8,8],[12,15],[17,20],[23,34]]           
-print(find_index(l,6,22))
+            if(index_one <len_list and added is False):
+                a,b = self.list[index_one][0],self.list[index_one][1]
+                while (range_intersection(a,b,c,d)):
+                    added = True
+                    r1,r2 = merge_ranges(a,b,c,d)
+                    del self.list[index_one] 
+                    if(index_one+1>len(self.list) ):
+                        break
+                    a,b = self.list[index_one][0],self.list[index_one][1]
+                    c,d = r1,r2
+                if(added is True):
+                    if r2>self.max_Range:
+                        insert_index = find_index(self.list,len(self.list),r1)
+                        self.total_sad += (r2-self.max_Range )*sadness
+                        self.list.insert(insert_index,[r1,self.max_Range])
+                    else:
+                        insert_index = find_index(self.list,len(self.list),r1)
+                        self.list.insert(insert_index,[r1,r2])
 
-print(range_intersection(3,8,8,13))
-print(merge_ranges(3,8,8,13))
+            if(added is False):
+                if d>self.max_Range:
+                    self.total_sad += (d-self.max_Range)*sadness
+                    self.list.insert(index,[c,self.max_Range])
+                else:
+                    self.list.insert(index,[c,d])
+
+    def print_var(self):
+        print("LIST =",self.list)
+        print("TOTAL SADNESS =",self.total_sad)
+        print("RANGE ALLOWED = ",self.max_Range)
+
+
+
+# l = [[1,3],[5,6],[8,8],[12,15],[17,20],[23,34]]           
+# print(find_index(l,6,8))
+
+# print(range_intersection(3,4,1,5))
+# print(merge_ranges(3,4,1,5))
+
+# tmp = add_lec(5)
+# tmp.add_sadness(1,1,150)
+# tmp.print_var()
+# tmp.add_sadness(5,2,200)
+# tmp.print_var()
+# tmp.add_sadness(2,4,100)
+# tmp.print_var()
+
+# my_dict = defaultdict(list)
+# #my_dict[200]=[[2,5]]
+# my_dict[400].append([5,5])
+# my_dict[200].append([7,8])
+# for k,v in sorted(my_dict.items()):
+#     for j in v:
+#         print(j)
+# print(my_dict)
+
+n  = int(input())
+for i in range(n):
+    l1,l2 = map(int, input().split(" "))   
+    my_dict = defaultdict(list)
+    rep = add_lec(l2)
+    for r in range(l1):
+        start,lect,sadness = map(int, input().split(" "))
+        my_dict[sadness].append([start,lect])
+    for k,v in sorted(my_dict.items(),reverse=True):
+        for j in v:
+            rep.add_sadness(j[0],j[1],k)
+    print(rep.total_sad)
