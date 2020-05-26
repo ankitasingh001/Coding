3d326dda4532f27620b8c1cc214cba45ad7fa5ca
= <ankitasinghchikki@gmail.com>
2020-05-26 21:18:37 +0530

    Rotated array finding minimum in O(logn)

--- Leetcode/find-minimum-in-rotated-sorted-array.py
+++ Leetcode/find-minimum-in-rotated-sorted-array.py
@@ -10,20 +10,22 @@ class Solution:
     def findMin(self, nums):
         l=0
         r = len(nums)-1
+        if(r==0):
+            return nums[r]
         while(l<r):
+            if (l == r+1)or (r== l+1) :
+                return min(nums[l],nums[r])
             mid = (l+r) //2
             if nums[mid]>nums[r]:
                 l = mid
             else:
                 r = mid
-            if (l == r+1)or (r== l+1):
-                return min(nums[l],nums[r])
-
+            
 # Test cases 
 
 S = Solution()
-print(S.findMin([3,4,5,1,2] ))
-print(S.findMin([4,5,6,7,0,1,2]))   
+print(S.findMin([1]))
+print(S.findMin([0,1,2,3,4,5,6,7]))   
 
 
 
