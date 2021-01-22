c1d9294808c6486af68be62bcf5781cfd4ff8d8c
= <ankitasinghchikki@gmail.com>
2021-01-22 22:19:41 +0530

    3 sum few test cases covered

--- Leetcode/C++/3sum.cpp
+++ Leetcode/C++/3sum.cpp
@@ -4,6 +4,12 @@
 
 */
 
+/*
+Please read the following for maps and undordered maps
+https://www.geeksforgeeks.org/map-vs-unordered_map-c/
+
+*/
+
 struct node
 {
     int val =0;
@@ -15,29 +21,56 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> triplets;
         set<vector<int> > set_of_vectors; 
-        sort(nums.begin(),nums.end());
+        //sort(nums.begin(),nums.end());
         map<int,int> intmap;
+        int counter =0;
         for (int i=0;i<nums.size();i++)
         {
             intmap[nums[i]] +=1;
         }
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]>=0)
+        for(auto i = intmap.begin();i!= intmap.end();i++)
+        {   
+            counter++;
+            if(i->first>0)
                 break;
-            for(int j=i+1;j<nums.size();j++)
+            for (auto j = next(i);j!= intmap.end();j++)
             {
-                int subint= -(nums[i]+nums[j]);
-                int hashint = intmap[subint];
-                if((hashint ==1 && nums[i]!= subint && nums[j]!= subint )) //|| (hashint>1 && (nums[i]==subint || nums[j]==subint))
+                
+                if (i->second>=1)
+                {
+                    if(j->first>0)
+                        break;
+                    int subint= -(i->first+ j->first);
+                    int hashint = intmap[subint];
+                    //cout<<"here i am "<<i->first<<" "<<j->first<<" "<<hashint<<endl;
+                    if((hashint >0))
                     {
-                        vector<int> triplet{nums[i],nums[j],-(nums[i]+nums[j])};
+                        vector<int> triplet{i->first,j->first,-(i->first+ j->first)};
                         set_of_vectors.insert(triplet);
-                        //cout<<nums[i]<<" "<<nums[j]<<" "<<-(nums[i]+nums[j])<<endl;
                     }
-
+                }
+                if (i->second >1)
+                {
+                    /* code */
+                    //cout<<"greater = "<<i->first<<endl;
+                    int subint= -(i->first)*2;
+                    int hashint = intmap[subint];
+                    if((hashint >0))
+                    {
+                        vector<int> triplet{i->first,i->first,-(i->first+ i->first)};
+                        set_of_vectors.insert(triplet);
+                    }
+                }
+               
+                
             }
+             if(i->second >2 && i->first==0)
+                    {
+                        vector<int> triplet{0,0,0};
+                        set_of_vectors.insert(triplet);
+                    }
         }
+        //cout<<"value of counter = "<<counter<<endl;
         triplets.assign(set_of_vectors.begin(),set_of_vectors.end());
         return triplets;
     }
@@ -48,8 +81,9 @@ int main()
 {
 	Solution s;
     vector<int> vect{-1,0,1,2,-1,-4}; 
+    vector<int> vec1{0,0,0};
     string tmp = "ngxlkthsjuoqcpavbfdermiywz";
-	vector<vector<int>> vec=s.threeSum(vect);
+	vector<vector<int>> vec=s.threeSum(vec1);
       
     // Displaying the 2D vector 
     for (int i = 0; i < vec.size(); i++) { 
