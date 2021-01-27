cf507bcc71a363dd5f1034112233543f2590d2dc
= <ankitasinghchikki@gmail.com>
2021-01-27 22:20:30 +0530

    Minor change for 0 case-> counting bits in O(n) time and O(n) space

--- Leetcode/C++/counting-bits.cpp
+++ Leetcode/C++/counting-bits.cpp
@@ -17,9 +17,8 @@ public:
         //declare a vector of size num and initialise with 0
         vector<int> v;
         v.push_back(0);
-        v.push_back(1);
         int k=0;
-        for(int i=2;i<=num;i++)
+        for(int i=1;i<=num;i++)
         {
             if(!(i&(i-1))) //number is a power of 2 or not
             {
