2ccbe480f6c73d00d4ce7e69a0c0d4638936748e
= <ankitasinghchikki@gmail.com>
2021-02-01 22:19:17 +0530

    coin change (2)

--- Leetcode/C++/coin-change.cpp
+++ Leetcode/C++/coin-change.cpp
@@ -12,18 +12,17 @@ using namespace std;
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> arr(amount+1,999999999);
+        vector<int> arr(amount+1,-1);
         arr[0]=0;
         for(int i=0;i<coins.size();i++)
         {
             arr[coins[i]]=1;
         }
-        for(int i=1;i<=amount;i++)
+        for(int i=0;i<coins.size();i++)
         {
-            for(int j=0;coin[j]<i;j++)
+            for(int j=1;j<=amount;j++)
             {
-                int coin = coins[j];
-                arr[i] = min(arr[i],1+arr[i-coin]);
+
             }
         }
     }
