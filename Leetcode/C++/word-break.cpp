bdc3f47f130b96e17a4f0d63a078876cbdfee0ce
= <ankitasinghchikki@gmail.com>
2021-02-17 22:37:20 +0530

    Word break final

--- Leetcode/C++/word-break.cpp
+++ Leetcode/C++/word-break.cpp
@@ -29,27 +29,31 @@ public:
         int k=0,tmp=0;
         for(int i=0;i<wordDict.size();i++)
         {
-            cout<<wordDict[i]<<endl;
+            //cout<<wordDict[i]<<endl;
             dictionary[wordDict[i]]=true;
         }
         for(int i=0;i<len;i++)
-            arr[i][i] = dictionary[to_string(s[i])];
+        {
+            string n(1,s[i]);
+            arr[i][i] = dictionary[n];
+            //cout<<(arr[i][i]);
+        }
         tmp=1;
         for(int i=len-1;i>=0;i--)
         {
             k=tmp;
             for(int j=0;j<=i;j++)
             {   
-                cout<<"values = "<<j<<" "<<k<<" ";
-                //cout<<arr[j][k-1]<<" "<<arr[j+1][k]<<" "<<s.substr(j,k-j+1)<<" ";
-                //cout<<isInDictionary(s.substr(j,k-j+1),dictionary)<<" end ";;
-                if((arr[j][k-1]||arr[j+1][k])||dictionary[s.substr(j,k-j+1)]) 
+                bool exists = false;
+                for(int l=j;l<k;l++)
+                    exists = exists || (arr[j][l]&&arr[l+1][k]);
+                if((exists)||dictionary[s.substr(j,k-j+1)]) 
                     arr[j][k]= true;
-                cout<<arr[j][k]<< " ";
+                //cout<<arr[j][k]<< " ";
                 k++;
             }
             tmp++;
-            cout<<endl;
+            //cout<<endl;
         }
         return arr[0][len];
     }
@@ -59,7 +63,7 @@ public:
 int main()
 {
     Solution s;
-    vector<string> vec{"cats", "dog", "sand", "and", "cat"};
-    string str="catsandog";
+    vector<string> vec{"a", "b"};
+    string str="ab";
     cout<<s.wordBreak(str,vec);
 }
